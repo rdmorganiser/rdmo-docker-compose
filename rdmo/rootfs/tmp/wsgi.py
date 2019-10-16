8afb7b80d10c8b0401531dfceee164645f107853
Olaf Michaelis <omichaelis@aip.de>
2019-10-16 17:36:40 +0200

    Add modified wsgi.py to avoid corrupt urls

--- rdmo/rootfs/tmp/wsgi.py
+++ rdmo/rootfs/tmp/wsgi.py
@@ -1,7 +1,16 @@
 import os
+import re
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+try:
+    os.environ['BASE_URL']
+except KeyError:
+    pass
+else:
+    if bool(re.search('[a-z0-9]', os.environ['BASE_URL'])) is False:
+        os.environ['BASE_URL'] = ''
 
 application = get_wsgi_application()
