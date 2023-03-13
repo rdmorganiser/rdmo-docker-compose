8c4efb0a39276c2c2245bd8a2c0641e488d81cc0
olaf michaelis <o.mic@web.de>
2023-03-13 12:34:04 +0100

    Fix local.py overwrite, do not replace

--- docker/rdmo/rootfs/home/rdmo/sh/install-rdmo-app.sh
+++ docker/rdmo/rootfs/home/rdmo/sh/install-rdmo-app.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 echo "Install RDMO App"
-git clone ${RDMO_APP_REPO} ${RDMO_APP}
-cp -f /conf/template_local.py ${RDMO_APP}/config/settings/local.py
+git clone "${RDMO_APP_REPO}" "${RDMO_APP}"
 
-pip install -r ${RDMO_APP}/requirements/gunicorn.txt
+targ="${RDMO_APP}/config/settings/local.py"
+if [[ ! -f "${targ}" ]]; then
+    cp -f /conf/template_local.py "${targ}"
+fi
 
-cd ${RDMO_APP}
+pip install -r "${RDMO_APP}/requirements/gunicorn.txt"
+
+cd "${RDMO_APP}" || exit 1
 python manage.py migrate
 python manage.py download_vendor_files
 python manage.py collectstatic --no-input
