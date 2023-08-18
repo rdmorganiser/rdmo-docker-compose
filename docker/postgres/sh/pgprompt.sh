b9046ff3f4e7c4a76f26ecf762bef52f6470ddd0
olaf michaelis <o.mic@web.de>
2023-08-18 08:05:04 +0200

    Add pgprompt script

--- docker/postgres/sh/pgprompt.sh
+++ docker/postgres/sh/pgprompt.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 
-fil="${1}"
-
-if [[ -z "${fil}" ]]; then
-  echo -e "\nplease provide a psql dump file\n"
-  exit 1
-fi
-
 export PGHOST="${POSTGRES_HOST}"
 export PGPORT="${POSTGRES_PORT}"
 export PGDATABASE="${POSTGRES_DB}"
 export PGUSER="${POSTGRES_USER}"
 export PGPASSWORD="${POSTGRES_PASSWORD}"
 
-createdb "${PGDATABASE}" || exit 1
-psql <"${fil}"
+psql
